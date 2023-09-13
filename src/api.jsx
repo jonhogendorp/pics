e35da2fa6fe03c457c77a74cbee66dd9756163a3
Jon Hogendorp <jonhoog@live.nl>
2023-09-13 22:58:45 +0200

    searching api

--- src/api.jsx
+++ src/api.jsx
@@ -5,7 +5,7 @@ const apiKey = 'YzXdKA-Gv1udacguQL4ZU-7BeHK6LeRaWOBvnguvjaE'
 const searchImages = async (term) => {
     const response = await axios.get('https://api.unsplash.com/search/photos',{
         headers:{
-            Authorization: `Client-ID`+ apiKey``,
+            Authorization: `Client-ID YzXdKA-Gv1udacguQL4ZU-7BeHK6LeRaWOBvnguvjaE`,
         },
         params:{
             query: term,
