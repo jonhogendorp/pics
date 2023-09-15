01bc14662c135178728b92e5f7870f4d65ed6ac3
Jon Hogendorp <jonhoog@live.nl>
2023-09-15 22:00:41 +0200

    finding the images

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import './SearchBar.css';
 
 function SearchBar({onSubmit}){
     const [term, setTerm] = useState('');
@@ -11,8 +12,9 @@ function SearchBar({onSubmit}){
         setTerm(event.target.value)
     }
     return (
-    <div> 
+    <div className="search-bar"> 
        <form onSubmit={handleFormSubmit}>
+        <label>Enter search term</label>
         <input value={term} onChange={handleChange} />
        </form>
     </div>
